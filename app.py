66f1bb3756266032aa5a0dac3abb0bd5dcb30c9c
mootuiy9845 <52738546+mootuiy9845@users.noreply.github.com>
2019-09-04 18:14:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
- [' originaldetectintentrequest '][' โหลด '][' ข้อมูล '][' replytoken ']
-[' queryresult '][' เจตนา '][' name ']
-user = decoded["events"][0]['replyToken'] 
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     if (userTaxt == 'กินข้าวหรือยัง') :
